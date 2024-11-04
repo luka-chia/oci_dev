dd89ba8ed1024a86ac9e94c8ca84733a5fb452ee
lulujia <lulujia@LULUJIA-7420.cn.oracle.com>
2024-11-04 16:06:59 +0800

    update from Windows 2024-11-04

--- OCI_SDK/vault/key_utils.py
+++ OCI_SDK/vault/key_utils.py
@@ -42,6 +42,33 @@ def encode_b64(text):
     encoded_data = base64.b64encode(text.encode('utf-8'))
     print(f"Encoded: {encoded_data.decode('utf-8')}")
     return encoded_data.decode('utf-8')
+def verify_data(text, message):
+    verify_response = key_management_client.verify(
+    verify_data_details=oci.key_management.models.VerifyDataDetails(
+        key_id="ocid1.key.oc1.ap-singapore-1.gzs6a4j6aabvg.abzwsljrp3mvz5tsgyqcfjtc43cxqgx2fqjw3dcmjewocvnrnnnatzxbn7sa",
+        signature=text,
+        message=message,
+        signing_algorithm="SHA_256_RSA_PKCS_PSS",
+        message_type="RAW"))
+
+    # Get the data from response
+    print(verify_response.data)
+
+def sign_data(text=None):
+    text="aGVsbG8sIHdvcmxk"
+    sign_response = key_management_client.sign(
+    sign_data_details=oci.key_management.models.SignDataDetails(
+        message=text,
+        key_id="ocid1.key.oc1.ap-singapore-1.gzs6a4j6aabvg.abzwsljrp3mvz5tsgyqcfjtc43cxqgx2fqjw3dcmjewocvnrnnnatzxbn7sa",
+        signing_algorithm="SHA_256_RSA_PKCS_PSS",
+        message_type="RAW"))
+
+    # Get the data from response
+    print(sign_response.data)
+    print("begin verify ===========================================================")
+    signature = sign_response.data.signature
+    verify_data(signature, text)
+
 
 if __name__ == "__main__":
     #results = search_by_user("luka", "OracleCanton10##")
@@ -50,8 +77,8 @@ if __name__ == "__main__":
     #plaintext = encode_b64(origin_text)
     #plaintext = u"L2FwaS9teWJhbmsvb3NwYXkvZW50ZXJwcmlzZS9nbG9iYWxjYXNobWFuYWdlbWVudC9tb2VnZ2xvYmFscGF5c3VibWl0L1YxP2FwcF9pZD0xMTAwMDAwMDAwMDAwMDAxNTM1MSZiaXpfY29udGVudD17dHJhbnNfY29kZToiR0xPQkFMUEFZIix0cmFuX2RhdGU6IjIwMTkwMTAxIix0cmFuX3RpbWU6IjEwMzIzMTAwMSIsbGFuZ3VhZ2U6InpoX0NOIixmX3NlcV9ubzoiR0xPQkFMUEFZMjAzNzYxMzU5MyIsemlwX2ZsYWc6IjAiLHppcDoiVUVzREJCUUFDQWciLHRvdGFsX251bToxLHRvdGFsX2FtdDoxMDB9bXNnX2lkPXVyY25sMjRjaXV0cjkmZm9ybWF0PWpzb24mY2hhcnNldD11dGYtOCZzaWduX3R5cGU9Q0EmJnRpbWVzdGFtcD0yMDI0LTEwLTMxIDA4OjAwOjAwIg=="
     #decode_b64(plaintext)
-    encrypt_data("L2FwaS9teWJhbmsvb3NwYXkvZW50ZXJwcmlzZS9nbG9iYWxjYXNobWFuYWdlbWVudC9tb2VnZ2xvYmFscGF5c3VibWl0L1YxP2FwcF9pZD0xMTAwMDAwMDAwMDAwMDAxNTM1MSZiaXpfY29udGVudD17dHJhbnNfY29kZToiR0xPQkFMUEFZIix0cmFuX2RhdGU6IjIwMTkwMTAxIix0cmFuX3RpbWU6IjEwMzIzMTAwMSIsbGFuZ3VhZ2U6InpoX0NOIixmX3NlcV9ubzoiR0xPQkFMUEFZMjAzNzYxMzU5MyIsemlwX2ZsYWc6IjAiLHppcDoiVUVzREJCUUFDQWciLHRvdGFsX251bToxLHRvdGFsX2FtdDoxMDB9bXNnX2lkPXVyY25sMjRjaXV0cjkmZm9ybWF0PWpzb24mY2hhcnNldD11dGYtOCZzaWduX3R5cGU9Q0EmJnRpbWVzdGFtcD0yMDI0LTEwLTMxIDA4OjAwOjAwIg==")
-    
+    #encrypt_data("L2FwaS9teWJhbmsvb3NwYXkvZW50ZXJwcmlzZS9nbG9iYWxjYXNobWFuYWdlbWVudC9tb2VnZ2xvYmFscGF5c3VibWl0L1YxP2FwcF9pZD0xMTAwMDAwMDAwMDAwMDAxNTM1MSZiaXpfY29udGVudD17dHJhbnNfY29kZToiR0xPQkFMUEFZIix0cmFuX2RhdGU6IjIwMTkwMTAxIix0cmFuX3RpbWU6IjEwMzIzMTAwMSIsbGFuZ3VhZ2U6InpoX0NOIixmX3NlcV9ubzoiR0xPQkFMUEFZMjAzNzYxMzU5MyIsemlwX2ZsYWc6IjAiLHppcDoiVUVzREJCUUFDQWciLHRvdGFsX251bToxLHRvdGFsX2FtdDoxMDB9bXNnX2lkPXVyY25sMjRjaXV0cjkmZm9ybWF0PWpzb24mY2hhcnNldD11dGYtOCZzaWduX3R5cGU9Q0EmJnRpbWVzdGFtcD0yMDI0LTEwLTMxIDA4OjAwOjAwIg==")
+    sign_data()
 
 
 
